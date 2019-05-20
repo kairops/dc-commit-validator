252a43f6ee798323d30f58e36c1fc30f490adf7b
Pedro Amador <pedroamador.rodriguez@gmail.com>
2019-05-20 17:55:02 +0200

    New: Initial import

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# dc-commit-validator
-Docker Command: Commit validator
+# Docker Command: Commit Validator
+
+Simply Commit Message Validator written in BASH
+
+Its a part of the Docker Command series
+
+## Usage
+
+Execute the following within your repository folder:
+
+- Using Bash:
+  - `./entrypoint.sh [git-repository-dir]`
+  - `./entrypoint.sh "Commit message"`
+  - `echo "Commit message" | ./entrypoint.sh -`
+- Using Docker:
+  - `docker run --rm -v [git-repository-dir]:/workspace kairops/dc-commit-validator .`
+  - `docker run --rm kairops/dc-commit-validator "Commit message"`
+  - `echo "Commit message" | docker run --rm kairops/dc-commit-validator -`
+- Using docker-command-launcher:
+  - `kd git-changelog-generator [git-repository-dir]`
+  - `kd git-changelog-generator "Commit message"`
+  - `echo "Commit message" | kd git-changelog-generator -`
+
+The script will check if the message(s) of the unreleased commit(s) (or the commit message passed as parameter) fulfill the rules of [Git Changelog Generator](https://github.com/kairops/dc-git-changelog-generator)
+
+- If you pass a existent directory of the filesystem in the first parameter, the script expect to find a git repository and will check all the messages of all unreleased commit.
+- If you pass a commit message as a text, the script will check it.
+
+In both cases, it will return 0 if the message (or messages) fulfill the rules, and 1 in other case.
+
+## Considerations
+
+Make the commits on your repository following [Keep Changelog](https://keepachangelog.com/en/1.0.0/) rules and these keywords:
+
+- Breaking - for a backwards-incompatible enhancement.
+- New - implemented a new feature.
+- Upgrade - for a dependency upgrade.
+- Update - for a backwards-compatible enhancement.
+- Fix - for a bug fix.
+- Build - changes to build process only.
+- Docs - changes to documentation only.
+- Security - for security skills.
+- Deprecated - for deprecated features.
---
+++ entrypoint.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Valid categories sorted in relevance order
+#
+#   Breaking - for a backwards-incompatible enhancement.
+#   New - implemented a new feature.
+#   Upgrade - for a dependency upgrade.
+#   Update - for a backwards-compatible enhancement.
+#   Fix - for a bug fix.
+#   Build - changes to build process only.
+#   Docs - changes to documentation only.
+#   Security - for security skills.
+#   Deprecated - for deprecated features.
+
+set -eo pipefail
+
+function echo_err () {
+    echo >&2 -e "$@"
+}
+function echo_debug () {
+    if [ "$KD_DEBUG" == "1" ]; then
+        echo >&2 ">>>> DEBUG >>>>> $(date "+%Y-%m-%d %H:%M:%S") $KD_NAME: $@"
+    fi
+}
+KD_NAME="commit-validator"
+echo_debug "begin"
+
+function getNumberByType() {
+    case $1 in
+        'Breaking:') number=0;;
+        'New:') number=1;;
+        'Upgrade:') number=2;;
+        'Update:') number=3;;
+        'Fix:') number=4;;
+        'Build:') number=5;;
+        'Docs:') number=6;;
+        'Security:') number=7;;
+        'Deprecated:') number=8;;
+        *) number=-1;;
+    esac
+    echo -n $number
+}
+
+function validateCommit() {
+    local commit=$1
+    echo_debug "Checking commit $commit"
+    type=$(echo $commit|awk '{print $2}')
+    message=$(echo $commit|awk '{ print substr($0, index($0,$3)) }')
+    number=$(getNumberByType $type)
+    if [ $number -eq -1 ]; then
+        echo_err "Error: Commit messager does not fulfill rules in commit '${commit}'"
+        echo "$commitList"
+        echo_debug "end"
+        exit 1
+    fi
+}
+
+function validateRepository() {
+    local lastTag=$(git describe --abbrev=0 --tags 2> /dev/null || echo '')
+    if [ "$lastTag" == "" ]; then
+        local commitRange="HEAD"
+    else
+        local commitRange="${lastTag}..HEAD"
+    fi
+
+    echo_debug "Using commit messages of '${commitRange}'"
+
+    # Initalizacion
+    OLDIFS="$IFS"
+    IFS=$'\n' # bash specific
+    commitList=$(git log ${commitRange} --no-merges --pretty=format:"%h %s")
+    for commit in $commitList
+    do
+        validateCommit $commit
+    done
+    echo_debug "All commits OK"
+    IFS="$OLDIFS"
+}
+
+# Parameter check
+if [ $# -eq 0 ]; then
+    echo_err "validate-commit: You should use [dirname], 'Commit message' as parameter or '-' to read from stdin."
+    echo_debug "end"
+    exit 1
+fi
+
+# Check parameter
+if [ "$1" == "-" ]; then
+    # Generate HTML from stdin
+    commitMessage=$(while IFS= read -r line; do
+        printf '%s\n' "$line"
+    done)
+    validateCommit "------- $commitMessage"
+    echo_debug "Commit message OK"
+else
+    if [ -d "$1" ]; then
+        validateRepository $1
+    else
+        commitMessage=$1
+        validateCommit "------- $commitMessage"
+        echo_debug "Commit message OK"
+    fi
+fi
+echo_debug "end"
